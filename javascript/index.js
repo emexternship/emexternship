3cd27570cf1c3a4ea179466f29f7633f5bea5864
emexternship <emmlakic@gmail.com>
2016-05-30 12:45:51 -0400

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,8 @@
-confirm("Are you ready to have fun?")
+confirm("I am ready to play!")
+var age = prompt("What's your age?"); 
+if (age <= 13) {
+    console.log("You are allowed to play, but we take no responsibility of your actions");
+}
+else { 
+    console.log("You're in! Let's begin!");
+}
