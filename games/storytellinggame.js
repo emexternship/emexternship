3207164354b8664d01bf32686d87b9b73885c392
emexternship <emmlakic@gmail.com>
2016-06-05 15:06:17 -0400

    Create storytellinggame.js

---
+++ games/storytellinggame.js
@@ -0,0 +1,11 @@
+confirm("I am ready to play!")
+var age = prompt("What's your age?"); 
+if (age <= 13) {
+    console.log("You are allowed to play, but we take no responsibility of your actions");
+}
+else { 
+    console.log("You're in! Let's begin!");
+}
+
+
+    
