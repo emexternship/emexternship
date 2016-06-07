a01279bf475be6762e5483742815bc9f199ba768
emexternship <emmlakic@gmail.com>
2016-06-06 22:12:24 -0400

    Update storytellinggame.js

--- games/storytellinggame.js
+++ games/storytellinggame.js
@@ -1,13 +1,2 @@
-confirm("I am ready to play!")
-var age = prompt("What's your age?"); 
-if (age <= 13) {
-    console.log("You are allowed to play, but we take no responsibility of your actions");
-}
-else { 
-    console.log("You're in! Let's begin!");
-}
-console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'"); 
-console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'"); 
-var userAnswer = prompt("Do you want to race Bieber on stage?") 
 
     
